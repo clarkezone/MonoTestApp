9b1aade9a7457dfbac1e0cfff087f33e5faf4588
James Clarke <james@clarkezone.net>
2023-11-09 14:21:27 -0800

    Fix runtime break

--- MonoTestApp/ContentView.swift
+++ MonoTestApp/ContentView.swift
@@ -17,9 +17,11 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        Text("Hello")
+                        //Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
                     } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        Text("Hello")
+                        //Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
                 }
                 .onDelete(perform: deleteItems)
